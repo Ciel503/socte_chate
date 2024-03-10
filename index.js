b957836ef59adb227696831d660bc1d562c4864b
Franciel Mesquita <francielmsq@gmail.com>
2024-03-10 17:49:59 -0300

    teste

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+
 const express = require('express')
 const path = require('path')
-const socketIo = require('socket.io')
 
 const app = express();
 
@@ -8,26 +8,6 @@ const app = express();
     app.use("/",express.static(path.join(__dirname,'public'))) //referencia a pasta public onde esta o index.html
 
 
-const server = app.listen(3000,()=>{
+app.listen(3000,()=>{
     console.log("rodando servidr")
 })
-const arreyMenssagens = []
-
-const io = socketIo(server)
-
-//  socket.on === recebe do body 
-// socket.emit === enviar pro body
-// io.emit === enviar pro body global
-// io.on === recebe do body global
-io.on('connection', (socket) => {
-    console.log('new_conection');
-    socket.emit("update_mensagens",arreyMenssagens) //quando new_conection envia o esse arrei pro front_and
-    
-    socket.on ('new_menssage',(dado)=>{  // recebe os dados do input do front-end 
-        arreyMenssagens.push(dado) // coloca os dados que foram recebidos do input no arrey
-        io.emit("update_mensagens",arreyMenssagens) //envia os dados do arrey para o fronte_and la na div]
-    })
-
-
-
-});
